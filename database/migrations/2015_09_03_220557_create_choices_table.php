d30749b5d923fdb7bba6f291d1982bb0a06d40cd
Josh Mountain <joshmountain@gmail.com>
2015-09-23 10:45:23 -0700

    Moved timestamp columns to the end of the table

--- database/migrations/2015_09_03_220557_create_choices_table.php
+++ database/migrations/2015_09_03_220557_create_choices_table.php
@@ -9,10 +9,10 @@ class CreateChoicesTable extends Migration {
 	{
 		Schema::create('choices', function(Blueprint $table) {
 			$table->increments('id');
-			$table->timestamps();
 			$table->tinyInteger('order')->unsigned();
 			$table->string('text')->default('This is where choice text goes.');
 			$table->string('meter_effect')->default('chance');
+			$table->timestamps();
 		});
 	}
 

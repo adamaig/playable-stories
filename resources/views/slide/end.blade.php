ed0cc07d67bd39538d6558ed1e16f6eb2bb73e63
Brad Wiatr <me@bradwiatr.com>
2015-10-08 16:13:39 -0700

    Style choices now apply to end slide. [Fixes #28]

--- resources/views/slide/end.blade.php
+++ resources/views/slide/end.blade.php
@@ -1,38 +1,99 @@
 @extends('framework-slide')
     
 @section('header-include')
+    @if (!empty($slide->story->heading_font))
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family={{ $slide->story->heading_font }}">
+        <style>
+            h1, h2, h3, h4, h5, h6 {
+                font-family: '{{ $fonts[$slide->story->heading_font] }}';
+            }
+        </style>
+    @endif
+    @if (!empty($slide->story->body_font))
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family={{ $slide->story->body_font }}">
+        <style>
+            body {
+                font-family: '{{ $fonts[$slide->story->body_font] }}';
+            }
+        </style>
+    @endif
     <style>
+        /* Background color and font sizes/colors/alignment */
+        body {
+            background-color: {{ $slide->story->background_color }};
+            font-size: {{ $slide->story->body_font_size}}px;
+            color: {{ $slide->story->body_font_color}};
+        }
+            main {
+                text-align: {{ $slide->text_alignment }};
+            }
+        a, a:hover, a:visited, a:active, a:link {
+            color: {{ $slide->story->link_color}};
+        }
+        .text-primary {
+            color: {{ $slide->story->link_color}};
+        }
+        
+        /* Navbars */
         .navbar {
             display: none;
         }
-        .jumbotron {
-            text-align: center;
+        
+        /* Buttons */
+        .btn-primary,
+        .btn-primary:hover,
+        .btn-primary:visited,
+        .btn-primary:active,
+        .btn-primary:active:focus,
+        .btn-primary:active:hover,
+        .btn-primary:focus,
+        .btn-primary:link {
+            color: {{ $slide->story->button_text_color}};
+            background-color: {{ $slide->story->button_background_color}};
         }
+            .btn-primary:hover {
+                opacity: 0.90;
+                filter: alpha(opacity=90); /* For IE8 and earlier */
+            }
+            .btn-primary:active:hover {
+                opacity: 1.0;
+                filter: alpha(opacity=100); /* For IE8 and earlier */
+            }
     </style>
 @stop
 
 @section('content')
-    <div class="jumbotron jumbotron-full-page">
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-xs-6 col-sm-4 col-sm-offset-2">
-                    <p>{{ Session::get('story-'.$story->id.'-meter-1-name') }}</p>
-                    <hr />
-                    <h2>{{ Session::get('story-'.$story->id.'-meter-1-value') }}</h2>
+    
+    <main class="content">
+        <div class="container-valign-center">
+            <div class="container">
+            
+                <div class="row text-center">
+                    <div class="col-xs-6 col-sm-4 col-sm-offset-2">
+                        <p style="margin-bottom: 0;">{{ Session::get('story-'.$slide->story->id.'-meter-1-name') }}</p>
+                        <h2 class="text-primary" style="margin-top: 0;">
+                            @if ( Session::get('story-'.$slide->story->id.'-meter-1-type') == 'currency' ) {{ '$' }}@endif{{ Session::get('story-'.$slide->story->id.'-meter-1-value') }}@if ( Session::get('story-'.$slide->story->id.'-meter-1-type') == 'percentage' ){{ '%' }} @endif
+                        </h2>
+                    </div>
+                    <div class="col-xs-6 col-sm-4">
+                        <p style="margin-bottom: 0;">{{ Session::get('story-'.$slide->story->id.'-meter-2-name') }}</p>
+                        <h2 class="text-primary" style="margin-top: 0;">
+                            @if ( Session::get('story-'.$slide->story->id.'-meter-2-type') == 'currency' ) {{ '$' }}@endif{{ Session::get('story-'.$slide->story->id.'-meter-2-value')}}@if ( Session::get('story-'.$slide->story->id.'-meter-2-type') == 'percentage' ){{ '%' }} @endif
+                        </h2>
+                    </div>
                 </div>
-                <div class="col-xs-6 col-sm-4">
-                    <p>{{ Session::get('story-'.$story->id.'-meter-2-name') }}</p>
-                    <hr />
-                    <h2>{{ Session::get('story-'.$story->id.'-meter-2-value') }}</h2>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h1>{{ $heading }}</h1>
+                        {!! $text !!}
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-                    <h1>{{ $heading }}</h1>
-                    {!! $text !!}
-                    <p><a class="btn btn-primary btn-lg" href="/story/{{ $story->id }}" role="button">Play again</a></p>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <p><a class="btn btn-lg btn-primary" href="/story/{{ $story->id }}" role="button">Play again</a></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 @stop

6a4566e75d034a7e028c4382504f1b463b5acf20
Josh Mountain <joshmountain@gmail.com>
2015-10-13 16:04:22 -0700

    Changed delete button to cancel button on choice edit screen

--- resources/views/choice/edit.blade.php
+++ resources/views/choice/edit.blade.php
@@ -7,7 +7,7 @@
 @section('navbar-right')
     <div id="navbar" class="navbar-collapse collapse">
         <div class="navbar-right">
-            <a href="" class="navbar-btn btn btn-sm btn-default" target="_blank">Delete</a>
+            <a href="/slide/{{ $choice->slide->id }}/edit" class="navbar-btn btn btn-sm btn-default">Cancel</a>
             <a href="" class="navbar-btn btn btn-sm btn-primary" id="save-button">Save</a>
         </div>
     </div>

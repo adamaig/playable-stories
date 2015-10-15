296cdf3f86ab8c6559b237a5233470b48ececa8d
Josh Mountain <joshmountain@gmail.com>
2015-10-15 15:02:25 -0700

    Fonts have been moved into config file

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use Session;
 use File;
+use Config;
 
 use PlayableStories\Http\Requests;
 use PlayableStories\Http\Controllers\Controller;
@@ -18,16 +19,6 @@ use PlayableStories\OutcomeResult;
 
 class SlideController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        //
-    }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -46,17 +37,6 @@ class SlideController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -67,98 +47,7 @@ class SlideController extends Controller
         $story = Story::findOrFail($id);
         $slide = Slide::where('story_id', $story->id)->where('order', $order)->first();
 
-        $googleFontList = array(
-            'Abril Fatface' => array(
-                'link_code' => 'Abril+Fatface',
-                'css_name' => 'Abril Fatface',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Archivo Narrow' => array(
-                'link_code' => 'Archivo+Narrow:400italic,400,700italic,700',
-                'css_name' => 'Archivo Narrow',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Arvo' => array(
-                'link_code' => 'Arvo:400italic,400,700italic,700',
-                'css_name' => 'Arvo',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Crete Round' => array(
-                'link_code' => 'Crete+Round:400italic,400',
-                'css_name' => 'Crete Round',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Droid Serif' => array(
-                'link_code' => 'Droid+Serif:400italic,400,700italic,700',
-                'css_name' => 'Droid Serif',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Josefin Slab' => array(
-                'link_code' => 'Josefin+Slab:400,400italic,700,700italic',
-                'css_name' => 'Josefin Slab',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Lato' => array(
-                'link_code' => 'Lato:400italic,400,700,700italic',
-                'css_name' => 'Lato',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Merriweather' => array(
-                'link_code' => 'Merriweather:400italic,400,700italic,700',
-                'css_name' => 'Merriweather',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Open Sans' => array(
-                'link_code' => 'Open+Sans:400,400italic,700italic,700',
-                'css_name' => 'Open Sans',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Oswald' => array(
-                'link_code' => 'Oswald:400,700',
-                'css_name' => 'Oswald',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto' => array(
-                'link_code' => 'Roboto:400,400italic,700,700italic',
-                'css_name' => 'Roboto',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto Condensed' => array(
-                'link_code' => 'Roboto+Condensed:400,400italic,700,700italic',
-                'css_name' => 'Roboto Condensed',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto Slab' => array(
-                'link_code' => 'Roboto+Slab:400,400italic,700,700italic',
-                'css_name' => 'Roboto Slab',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Ubuntu' => array(
-                'link_code' => 'Ubuntu:400,400italic,700,700italic',
-                'css_name' => 'Ubuntu',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Varela Round' => array(
-                'link_code' => 'Varela+Round',
-                'css_name' => 'Varela Round',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-        );
+        $googleFontList = Config::get('fonts');
 
         return view('slide.show')->withStory($story)->withSlide($slide)->withFonts($googleFontList);
     }
@@ -320,98 +209,7 @@ class SlideController extends Controller
         $slide = Slide::where('story_id', $story->id)->where('order', $order)->first();
         $vignette = null;
         
-        $googleFontList = array(
-            'Abril Fatface' => array(
-                'link_code' => 'Abril+Fatface',
-                'css_name' => 'Abril Fatface',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Archivo Narrow' => array(
-                'link_code' => 'Archivo+Narrow:400italic,400,700italic,700',
-                'css_name' => 'Archivo Narrow',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Arvo' => array(
-                'link_code' => 'Arvo:400italic,400,700italic,700',
-                'css_name' => 'Arvo',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Crete Round' => array(
-                'link_code' => 'Crete+Round:400italic,400',
-                'css_name' => 'Crete Round',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Droid Serif' => array(
-                'link_code' => 'Droid+Serif:400italic,400,700italic,700',
-                'css_name' => 'Droid Serif',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Josefin Slab' => array(
-                'link_code' => 'Josefin+Slab:400,400italic,700,700italic',
-                'css_name' => 'Josefin Slab',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Lato' => array(
-                'link_code' => 'Lato:400italic,400,700,700italic',
-                'css_name' => 'Lato',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Merriweather' => array(
-                'link_code' => 'Merriweather:400italic,400,700italic,700',
-                'css_name' => 'Merriweather',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Open Sans' => array(
-                'link_code' => 'Open+Sans:400,400italic,700italic,700',
-                'css_name' => 'Open Sans',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Oswald' => array(
-                'link_code' => 'Oswald:400,700',
-                'css_name' => 'Oswald',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto' => array(
-                'link_code' => 'Roboto:400,400italic,700,700italic',
-                'css_name' => 'Roboto',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto Condensed' => array(
-                'link_code' => 'Roboto+Condensed:400,400italic,700,700italic',
-                'css_name' => 'Roboto Condensed',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto Slab' => array(
-                'link_code' => 'Roboto+Slab:400,400italic,700,700italic',
-                'css_name' => 'Roboto Slab',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Ubuntu' => array(
-                'link_code' => 'Ubuntu:400,400italic,700,700italic',
-                'css_name' => 'Ubuntu',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Varela Round' => array(
-                'link_code' => 'Varela+Round',
-                'css_name' => 'Varela Round',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-        );
+        $googleFontList = Config::get('fonts');
         
         if ($choice->meter_effect == 'none') {
             if (count($story->slides()->get()) == $order) {
@@ -509,98 +307,7 @@ class SlideController extends Controller
      */
     public function end($id)
     {
-        $googleFontList = array(
-            'Abril Fatface' => array(
-                'link_code' => 'Abril+Fatface',
-                'css_name' => 'Abril Fatface',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Archivo Narrow' => array(
-                'link_code' => 'Archivo+Narrow:400italic,400,700italic,700',
-                'css_name' => 'Archivo Narrow',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Arvo' => array(
-                'link_code' => 'Arvo:400italic,400,700italic,700',
-                'css_name' => 'Arvo',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Crete Round' => array(
-                'link_code' => 'Crete+Round:400italic,400',
-                'css_name' => 'Crete Round',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Droid Serif' => array(
-                'link_code' => 'Droid+Serif:400italic,400,700italic,700',
-                'css_name' => 'Droid Serif',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Josefin Slab' => array(
-                'link_code' => 'Josefin+Slab:400,400italic,700,700italic',
-                'css_name' => 'Josefin Slab',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Lato' => array(
-                'link_code' => 'Lato:400italic,400,700,700italic',
-                'css_name' => 'Lato',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Merriweather' => array(
-                'link_code' => 'Merriweather:400italic,400,700italic,700',
-                'css_name' => 'Merriweather',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Open Sans' => array(
-                'link_code' => 'Open+Sans:400,400italic,700italic,700',
-                'css_name' => 'Open Sans',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Oswald' => array(
-                'link_code' => 'Oswald:400,700',
-                'css_name' => 'Oswald',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto' => array(
-                'link_code' => 'Roboto:400,400italic,700,700italic',
-                'css_name' => 'Roboto',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto Condensed' => array(
-                'link_code' => 'Roboto+Condensed:400,400italic,700,700italic',
-                'css_name' => 'Roboto Condensed',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Roboto Slab' => array(
-                'link_code' => 'Roboto+Slab:400,400italic,700,700italic',
-                'css_name' => 'Roboto Slab',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Ubuntu' => array(
-                'link_code' => 'Ubuntu:400,400italic,700,700italic',
-                'css_name' => 'Ubuntu',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-            'Varela Round' => array(
-                'link_code' => 'Varela+Round',
-                'css_name' => 'Varela Round',
-                'normal_weight' => 400,
-                'bold_weight' => 700,
-            ),
-        );
+        $googleFontList = Config::get('fonts');
 
         $story = Story::findOrFail($id);
         $slide = Slide::where('story_id', $story->id)->orderBy('order')->first();
---
+++ config/fonts.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+
+    'Abril Fatface' => array(
+        'link_code' => 'Abril+Fatface',
+        'css_name' => 'Abril Fatface',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Archivo Narrow' => array(
+        'link_code' => 'Archivo+Narrow:400italic,400,700italic,700',
+        'css_name' => 'Archivo Narrow',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Arvo' => array(
+        'link_code' => 'Arvo:400italic,400,700italic,700',
+        'css_name' => 'Arvo',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Crete Round' => array(
+        'link_code' => 'Crete+Round:400italic,400',
+        'css_name' => 'Crete Round',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Droid Serif' => array(
+        'link_code' => 'Droid+Serif:400italic,400,700italic,700',
+        'css_name' => 'Droid Serif',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Josefin Slab' => array(
+        'link_code' => 'Josefin+Slab:400,400italic,700,700italic',
+        'css_name' => 'Josefin Slab',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Lato' => array(
+        'link_code' => 'Lato:400italic,400,700,700italic',
+        'css_name' => 'Lato',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Merriweather' => array(
+        'link_code' => 'Merriweather:400italic,400,700italic,700',
+        'css_name' => 'Merriweather',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Open Sans' => array(
+        'link_code' => 'Open+Sans:400,400italic,700italic,700',
+        'css_name' => 'Open Sans',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Oswald' => array(
+        'link_code' => 'Oswald:400,700',
+        'css_name' => 'Oswald',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Roboto' => array(
+        'link_code' => 'Roboto:400,400italic,700,700italic',
+        'css_name' => 'Roboto',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Roboto Condensed' => array(
+        'link_code' => 'Roboto+Condensed:400,400italic,700,700italic',
+        'css_name' => 'Roboto Condensed',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Roboto Slab' => array(
+        'link_code' => 'Roboto+Slab:400,400italic,700,700italic',
+        'css_name' => 'Roboto Slab',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Ubuntu' => array(
+        'link_code' => 'Ubuntu:400,400italic,700,700italic',
+        'css_name' => 'Ubuntu',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+    'Varela Round' => array(
+        'link_code' => 'Varela+Round',
+        'css_name' => 'Varela Round',
+        'normal_weight' => 400,
+        'bold_weight' => 700,
+    ),
+
+];

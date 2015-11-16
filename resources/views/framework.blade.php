31fbf0939522df470abb89508e14fd407ebcdb7d
Josh Mountain <joshmountain@gmail.com>
2015-11-16 15:49:53 -0700

    Added sign out link

--- resources/views/framework.blade.php
+++ resources/views/framework.blade.php
@@ -5,17 +5,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        
+
         <title>Playable Stories</title>
 
         <!-- Bootstrap -->
         <link href="/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-        
+
         <!-- Font Awesome -->
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        
+
         <link rel="stylesheet" href="/css/app.css">
-        
+
         @yield('header-include')
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -25,7 +25,7 @@
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
-    
+
     <body>
         <!-- Static navbar -->
         <nav class="navbar navbar-default navbar-static-top">
@@ -38,19 +38,22 @@
                         <span class="icon-bar"></span>
                     </button>
                     <a class="navbar-brand" href="/">Playable Stories</a>
+                    @if(Auth::check())
+                        <a id="logout-link" href="/auth/logout">sign out</a>
+                    @endif
                 </div>
                 @yield('navbar-right')
             </div>
         </nav>
-        
+
         @yield('content')
-        
+
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-        
+
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
-        
+
         @yield('footer-include')
     </body>
 </html>

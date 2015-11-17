435007b2b3c1530555c4c6d31c1c309fa6d5f46e
Josh Mountain <joshmountain@gmail.com>
2015-11-17 11:26:14 -0700

    Fixed redirect

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,6 +24,7 @@ class AuthController extends Controller
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     protected $redirectPath = '/';
+    protected $loginPath = '/';
 
     /**
      * Create a new authentication controller instance.

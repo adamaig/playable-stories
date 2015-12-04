ddd686a384a88d9f850eeec1a048e330c28ac3c0
Josh Mountain <joshmountain@gmail.com>
2015-12-04 11:52:12 -0700

    Fixes #79

--- resources/views/framework-introduction.blade.php
+++ resources/views/framework-introduction.blade.php
@@ -5,17 +5,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        
+
         <title>{{ $introduction->story->name }}</title>
 
         <!-- Bootstrap -->
         <link href="/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-        
+
         <!-- Font Awesome -->
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        
+
         <link rel="stylesheet" href="/css/app.css">
-        
+
         @yield('header-include')
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -25,9 +25,9 @@
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
-    
+
     <body class="story-view">
-    
+
         <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container">
                 <div class="navbar-left">
@@ -38,23 +38,23 @@
                 </div>
             </div>
         </nav>
-        
+
         @yield('content')
-    
+
         <nav class="navbar navbar-default navbar-fixed-bottom">
             <div class="container">
                 <div class="navbar-center">
-                    <p class="navbar-text text-center">Powered by <a href="/">Playable Stories</a></p>
+                    <p class="navbar-text text-center">Powered by <a href="http://playablestories.org">Playable Stories</a></p>
                 </div>
             </div>
         </nav>
-        
+
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-        
+
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
-        
+
         @yield('footer-include')
     </body>
 </html>
--- resources/views/framework-slide.blade.php
+++ resources/views/framework-slide.blade.php
@@ -5,17 +5,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        
+
         <title>{{ $slide->story->name }}</title>
 
         <!-- Bootstrap -->
         <link href="/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-        
+
         <!-- Font Awesome -->
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        
+
         <link rel="stylesheet" href="/css/app.css">
-        
+
         <style>
             /* Format the meter styles with the global story styles. */
             .navbar-default .navbar-text {
@@ -25,7 +25,7 @@
                 color: {{ $slide->story->link_color}};
             }
         </style>
-        
+
         @yield('header-include')
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -35,7 +35,7 @@
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
-    
+
     <body class="story-view">
 
         <nav class="navbar navbar-default navbar-fixed-top">
@@ -60,23 +60,23 @@
                 </div>
             </div>
         </nav>
-        
+
         @yield('content')
-    
+
         <nav class="navbar navbar-default navbar-fixed-bottom">
             <div class="container">
                 <div class="navbar-center">
-                    <p class="navbar-text text-center" style="font-size: initial;"><small>Powered by <a href="/">Playable Stories</a></small></p>
+                    <p class="navbar-text text-center" style="font-size: initial;"><small>Powered by <a href="http://playablestories.org">Playable Stories</a></small></p>
                 </div>
             </div>
         </nav>
-        
+
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-        
+
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
-        
+
         @yield('footer-include')
     </body>
 </html>

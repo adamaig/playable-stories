2860168db590804f50f0740aaef14bb882f35dac
Josh Mountain <joshmountain@gmail.com>
2016-04-19 15:09:58 -0700

    Backup updates

--- config/laravel-backup.php
+++ config/laravel-backup.php
@@ -2,88 +2,172 @@
 
 return [
 
-    'source' => [
+    'backup' => [
 
-        'files' => [
+        /*
+         * The name of this application. You can use this name to monitor
+         * the backups.
+         */
+        'name' => 'playable-media',
 
-            /*
-             * The list of directories that should be part of the backup. You can
-             * specify individual files as well.
-             */
-            'include' => [
-                base_path(),
+        'source' => [
+
+            'files' => [
+
+                /*
+                 * The list of directories that should be part of the backup. You can
+                 * specify individual files as well.
+                 */
+                'include' => [
+                    base_path(),
+                ],
+
+                /*
+                 * These directories will be excluded from the backup.
+                 * You can specify individual files as well.
+                 */
+                'exclude' => [
+                    base_path('vendor'),
+                    base_path('node_modules'),
+                    storage_path(),
+                ],
             ],
 
             /*
-             * These directories will be excluded from the backup.
-             * You can specify individual files as well.
+             * The names of the connections to the databases that should be part of the backup.
+             * Currently only MySQL- and PostgreSQL-databases are supported.
              */
-            'exclude' => [
-                base_path('vendor'),
+            'databases' => [
+                'mysql',
             ],
         ],
 
-        /*
-         * Should the database be part of the back up.
-         */
-        'backup-db' => true,
+        'destination' => [
+
+            /*
+             * The disk names on which the backups will be stored.
+             */
+            'disks' => [
+                'sftp',
+            ],
+        ],
     ],
 
-    'destination' => [
-
+    'cleanup' => [
         /*
-         * The filesystem(s) you on which the backups will be stored. Choose one or more
-         * of the filesystems you configured in app/config/filesystems.php
+         * The strategy that will be used to cleanup old backups.
+         * The youngest backup will never be deleted.
          */
-        'filesystem' => ['sftp'],
+        'strategy' => \Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy::class,
+
+        'defaultStrategy' => [
+
+            /*
+             * The amount of days that all daily backups must be kept.
+             */
+            'keepAllBackupsForDays' => 7,
+
+            /*
+             * The amount of days that all daily backups must be kept.
+             */
+            'keepDailyBackupsForDays' => 16,
+
+            /*
+             * The amount of weeks of which one weekly backup must be kept.
+             */
+            'keepWeeklyBackupsForWeeks' => 8,
+
+            /*
+             * The amount of months of which one monthly backup must be kept.
+             */
+            'keepMonthlyBackupsForMonths' => 4,
+
+            /*
+             * The amount of years of which one yearly backup must be kept
+             */
+            'keepYearlyBackupsForYears' => 2,
+
+            /*
+             * After cleaning up the backups remove the oldest backup until
+             * this amount of megabytes has been reached.
+             */
+            //'deleteOldestBackupsWhenUsingMoreMegabytesThan' => 5000
+        ]
+    ],
+
+
+    /*
+     *  In this array you can specify which backups should be monitored.
+     *  If a backup does not meet the specified requirements the
+     *  UnHealthyBackupWasFound-event will be fired.
+     */
+    'monitorBackups' => [
+        [
+            'name' => 'playable-media',
+            'disks' => ['sftp'],
+            'newestBackupsShouldNotBeOlderThanDays' => 1,
+            //'storageUsedMayNotBeHigherThanMegabytes' => 5000,
+        ],
 
         /*
-         * The path where the backups will be saved. This path
-         * is relative to the root you configured on your chosen
-         * filesystem(s).
+        [
+            'name' => 'name of the second app',
+            'disks' => ['local', 's3'],
+            'newestBackupsShouldNotBeOlderThanDays' => 1,
+            'storageUsedMayNotBeHigherThanMegabytes' => 5000,
+        ],
+        */
+    ],
+
+    'notifications' => [
+
+        /*
+         * This class will be used to send all notifications.
+         */
+        'handler' => Spatie\Backup\Notifications\Notifier::class,
+
+        /*
+         * Here you can specify the ways you want to be notified when certain
+         * events take place. Possible values are "log", "mail", "slack" and "pushover".
          *
-         * If you're using the local filesystem a .gitignore file will
-         * be automatically placed in this directory so you don't
-         * accidentally end up committing these backups.
+         * Slack requires the installation of the maknz/slack package.
          */
-        'path' => '/Knownhost_Backups/_Laravel_App_Backups/playable-stories-stage/'. date('Y-m-d'),
+        'events' => [
+            'whenBackupWasSuccessful'     => ['log'],
+            'whenCleanupWasSuccessful'    => ['log'],
+            'whenHealthyBackupWasFound'   => ['log'],
+            'whenBackupHasFailed'         => ['log', 'mail'],
+            'whenCleanupHasFailed'        => ['log', 'mail'],
+            'whenUnhealthyBackupWasFound' => ['log', 'mail'],
+        ],
 
         /*
-         * By default the backups will be stored as a zipfile with a
-         * timestamp as the filename. With these options You can
-         * specify a prefix and a suffix for the filename.
+         * Here you can specify how emails should be sent.
          */
-        'prefix' => '',
-        'suffix' => '',
-    ],
-
-    'clean' => [
-        /*
-         * The clean command will remove all backups on all configured filesystems
-         * that are older then this amount of days.
-         */
-        'maxAgeInDays' => 90,
-    ],
-
-    'mysql' => [
-        /*
-         * The path to the mysqldump binary. You can leave this empty
-         * if the binary is installed in the default location.
-         */
-        'dump_command_path' => '',
+        'mail' => [
+            'from' => 'josh@user10.com',
+            'to'   => 'josh@user10.com',
+        ],
 
         /*
-         * If your server supports it you can turn on extended insert.
-         * This will result in a smaller dump file and speeds up the backup process.
-         *
-         * See: https://dev.mysql.com/doc/refman/5.1/en/mysqldump.html#option_mysqldump_extended-insert
+         * Here you can specify how messages should be sent to Slack.
          */
-        'useExtendedInsert' => false,
+        // 'slack' => [
+        //     'channel'  => '#backups',
+        //     'username' => 'Backup bot',
+        //     'icon'     => ':robot:',
+        // ],
 
         /*
-         * If the dump of the db takes more seconds that the specified value,
-         * it will abort the backup.
+         * Here you can specify how messages should be sent to Pushover.
          */
-        'timeoutInSeconds' => 180,
-    ],
+        // 'pushover' => [
+        //     'token'  => env('PUSHOVER_APP_TOKEN'),
+        //     'user'   => env('PUSHOVER_USER_KEY'),
+        //     'sounds' => [
+        //         'success' => env('PUSHOVER_SOUND_SUCCESS','pushover'),
+        //         'error'   => env('PUSHOVER_SOUND_ERROR','siren'),
+        //     ],
+        // ],
+    ]
 ];

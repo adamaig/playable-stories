1110e3ccec7cb235e6157d5c9bbb961c795a2563
Adam Ingram-Goble <adamaig@gmail.com>
2016-08-04 07:50:42 +0300

    Simpler brand in navbar

--- resources/views/framework.blade.php
+++ resources/views/framework.blade.php
@@ -46,7 +46,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="/">Playable Media: Story Builder</a>
+                    <a class="navbar-brand" href="/">Story Builder</a>
                     @if(Auth::check())
                         <div id="logout-link">
                             <a href="/">My Stories</a><a href="/auth/logout">Log Out</a>
